3f59e3b6d0ed0e7bc52790d9ccdc8750b1d5bdb6
dslee <dongsublee681@gmail.com>
2025-01-07 15:19:48 +0900

    tudoorlock.

--- app/src/main/java/com/project/tudoorlock/MainActivity.kt
+++ app/src/main/java/com/project/tudoorlock/MainActivity.kt
@@ -4,28 +4,34 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var productIdInput: EditText
+    private lateinit var submitButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // 메인 XML 연결
+        setContentView(R.layout.activity_main)
 
-        // "비밀번호 입력" 버튼
-        val passwordInputButton = findViewById<Button>(R.id.btn_password_input)
-        passwordInputButton.setOnClickListener {
-            // PasswordInputActivity로 전환
-            val intent = Intent(this, PasswordInputActivity::class.java)
-            startActivity(intent)
-            val backButton = findViewById<ImageButton>(R.id.btn_back)
-            backButton.setOnClickListener {
-                // MainActivity로 이동
-                val intent = Intent(this, MainActivity::class.java)
+        productIdInput = findViewById(R.id.product_id_input)
+        submitButton = findViewById(R.id.submit_button)
+
+        submitButton.setOnClickListener {
+            val input = productIdInput.text.toString().trim()
+
+            if (input == "666") {
+
+                val intent = Intent(this, MenuActivity::class.java)
                 startActivity(intent)
+            } else {
 
-                // 현재 Activity 종료
-                finish()
+                Toast.makeText(this, "잘못된 제품 아이디입니다.", Toast.LENGTH_SHORT).show()
             }
         }
     }
 }
+
---
+++ app/src/main/java/com/project/tudoorlock/MenuActivity.kt
@@ -0,0 +1,12 @@
+package com.project.tudoorlock
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu) // menu.xml 레이아웃 파일을 사용
+    }
+}
