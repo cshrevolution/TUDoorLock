df86460593a1b0f80308a8c4e6a8a47e30965935
dslee <dongsublee681@gmail.com>
2025-01-09 17:47:47 +0900

    tudoorlock

--- app/src/main/java/com/project/tudoorlock/MenuActivity.kt
+++ app/src/main/java/com/project/tudoorlock/MenuActivity.kt
@@ -11,10 +11,17 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu) // menu.xml 로드
 
-        // 설정 버튼 클릭 이벤트
+        //설정 버튼 클릭 이벤트
         val btnSettings: ImageButton = findViewById(R.id.btn_settings)
         btnSettings.setOnClickListener {
             // SettingsActivity로 이동
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+        //알림 버튼 클릭 이벤트
+        val btnNotification: ImageButton = findViewById(R.id.btn_notification)
+        btnSettings.setOnClickListener {
+            // NotificationActivity로 이동
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
         }
